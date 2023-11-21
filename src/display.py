bcf39a981fb088688c92c32f04d2d8016c7f989e
geckro <94733731+geckro@users.noreply.github.com>
2023-11-21 10:51:03 +0800

    Added constructors and attributes to the display and sensor classes

--- src/display.py
+++ src/display.py
@@ -1,2 +1,13 @@
 class Display:
-    pass
+    # NOTE:
+    # Renamed id to display_id to stop potential interference with id()
+    def __init__(self, display_id, carpark, message="", is_on=False):
+        self.id = display_id
+        self.message = message
+        self.is_on = is_on
+        self.carpark = carpark
+
+    def __str__(self):
+        # Added some list comprehension to this in case is_on is off
+        # Don't know exactly what "is on" refers to but i would assume this
+        return f"Display {self.id}, {self.message, self.carpark if self.is_on else 'off'}"
--- src/sensor.py
+++ src/sensor.py
@@ -1,2 +1,18 @@
 class Sensor:
+    # NOTE:
+    # Renamed id to sensor_id to stop potential interference with id()
+    def __init__(self, sensor_id, is_active, carpark):
+        self.id = sensor_id
+        self.is_active = is_active
+        self.carpark = carpark
+
+    def __str__(self):
+        return f"ID: {self.id}, carpark {self.is_active}, {self.carpark}"
+
+
+class EntrySensor(Sensor):
+    pass
+
+
+class ExitSensor(Sensor):
     pass
